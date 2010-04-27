f32fc4acf264415839dbd6155a33feffba8de7a5
unknown <teddydestodes@.(none)>
2010-04-27 21:10:37 +0200

    config change

--- etc/config.inc.php
+++ etc/config.inc.php
@@ -4,8 +4,8 @@
  */
 $_config['mysql-host'] = 'localhost';
 $_config['mysql-db']   = 'radio_testing';
-$_config['mysql-user'] = 'radiotest';
-$_config['mysql-pass'] = 'radio';
+$_config['mysql-user'] = 'radio';
+$_config['mysql-pass'] = 'qwertz123';
 
 $_config['template'] = 'radio';
 
