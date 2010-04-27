4c6d370254e68e515730234cc676f3c0171bbf36
unknown <teddydestodes@.(none)>
2010-04-27 21:01:09 +0200

    Changed paths

--- bin/liq_start.sh
+++ bin/liq_start.sh
@@ -2,7 +2,7 @@
 
 while [ ! -f ~/var/killliq ]
 do
-    liquidsoap ~/etc/radio.liq 2>> ~/var/log/liquidsoap_error.log;
+    liquidsoap /srv/radio/etc/radio.liq 2>> /srv/radio/var/log/liquidsoap_error.log;
 echo 'liquidsoap crashed ... restarting in 10s ...';
 sleep 10;
 echo 'restarting ...'
