784eb4c2b3af096cca5fce16e8bd5d5ad246936a
RadioServer <radio@radio.krautchan.net>
2010-04-28 19:40:39 +0000

    Bug #1 fixed

--- www/include/sidebar.php
+++ www/include/sidebar.php
@@ -4,19 +4,17 @@ $result = $db->query($sql);
 $usercount = $db->fetch($result);
 $template->assign('sb_streamercount',$usercount['count']);
 
-$sql = "SELECT artist,title FROM songhistory ORDER BY songid desc LIMIT 10;";
+$sql = "SELECT songid,artist,title FROM songhistory ORDER BY songid desc LIMIT 10;";
 $result = $db->query($sql);
 $songs = array();
 if($db->num_rows($result)){
     
     while($song = $db->fetch($result)){
-        if (strlen($song['artist']) >= 15) {
-		$song['artist'] = substr($song['artist'], 0, 5);
-		$song['artist'] .= '<a href="/history.php" title="Mehr!">...</a>';
-	}
-	if (strlen($song['title']) >= 15) {
-		$song['title'] = substr($song['title'], 0, 5);
-		$song['title'] .= '<a href="/history.php" title="Mehr!">...</a>';
+	$id = $song['songid'];
+	$song = $song['artist'] . " - " . $song['title'];
+        if (strlen($song) > 35) {
+		$song = trim(substr($song, 0, 32));
+		$song .= "<a href=\"/history.php?id={$id}\" title=\"Mehr!\">...</a>";
 	}
 	$songs[] = $song;
     }
