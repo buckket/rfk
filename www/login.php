19a1bc4bad98ee8185782696971675646ddd819b
kaarlo <kaarlo@radio.krautchan.net>
2010-04-28 23:05:19 +0000

    fixed menu selection marking

--- www/login.php
+++ www/login.php
@@ -3,6 +3,7 @@ require_once('../lib/common-web.inc.php');
 $template = new BpTemplate('login.html');
 include('include/listenercount.php');
 cleanup($template);
+$template->assign('section', "login");
 $template->assign('PAGETITLE', "Einloggen");
 echo $template->render();
 
--- www/register.php
+++ www/register.php
@@ -3,6 +3,7 @@ require_once('../lib/common-web.inc.php');
 $template = new BpTemplate('register.html');
 include('include/listenercount.php');
 
+$template->assign('section', "register");
 $template->assign('PAGETITLE', "Registrieren");
 
 if($_POST['action'] == 'register'){
