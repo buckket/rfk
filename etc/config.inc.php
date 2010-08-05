1036d70153d7258c409a62fa9938415585646327
teddydestodes <teddydestodes@denkunterlage.(none)>
2010-08-05 19:42:18 +0200

    sql für den webstuhl

--- etc/config.inc.php
+++ etc/config.inc.php
@@ -10,4 +10,12 @@ $_config['mysql-pass'] = 'muhmann';
 $_config['template'] = 'radio';
 
 $_config['default-username'] = 'Gast';
+
+$_config['icecasturl'] = 'localhost';
+
+$_config['icecastport'] = 8000;
+
+$_config['liquidsoap_address'] = '192.168.2.101';
+
+$_config['liquidsoap_port'] = 8010;
 ?>
