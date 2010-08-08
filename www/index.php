57810c4fefc112a75cfd1b73e298a1ae45092ca3
teddydestodes <derkomtur@schattengang.net>
2010-08-09 00:08:39 +0200

    DIE JUDENVERGASUNG

--- www/index.php
+++ www/index.php
@@ -1,8 +1,23 @@
 <?php
 require_once('../lib/common-web.inc.php');
 $template = array();
-$template['PAGETITLE'] = 'Übersicht';
+$template['PAGETITLE'] = 'Ãœbersicht';
 $template['section'] = 'overview';
+$sql = "SELECT news, time, description, text, username
+        FROM news
+        JOIN streamer USING (streamer)
+        ORDER BY time
+        DESC LIMIT 5;";
+$dbres = $db->query($sql);
+if($dbres){
+    while($row = $db->fetch($dbres)){
+        $template['news'][] = array('id' => $row['news'],
+                                    'description' => $row['description'],
+                                    'user' => $row['username'],
+                                    'text' => $row['text'],
+                                    'time' => $row['time']);
+    }
+}
 cleanup_h2o($template);
 include('include/listenercount.php');
 include('include/sidebar.php');
