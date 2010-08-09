63845b2a30e172af6a79137f9372113d02fbb624
teddydestodes <derkomtur@schattengang.net>
2010-08-09 22:27:17 +0200

    so nun können die idioten auch ihr passwort ändern

--- www/settings.php
+++ www/settings.php
@@ -6,40 +6,17 @@ if(!$user->logged_in ){
     exit();
 }
 if(isset($_POST['submit'])) {
-    if(isset($_POST['showname'])){
-        $sql = "INSERT INTO streamersettings (streamer, `key`, value) VALUES
-                (".$user->userid.",'defaultshowname','".$db->escape($_POST['showname'])."')
-                ON DUPLICATE KEY UPDATE value = '".$db->escape($_POST['showname'])."'";
-        $db->execute($sql);
-    }
-    if(isset($_POST['showdesc'])){
-        $sql = "INSERT INTO streamersettings (streamer, `key`, value) VALUES
-                (".$user->userid.",'defaultshowdescription','".$db->escape($_POST['showdesc'])."')
-                ON DUPLICATE KEY UPDATE value = '".$db->escape($_POST['showdesc'])."'";
-        $db->execute($sql);
-    }
-    if(isset($_POST['icy'])){
-        $sql = "INSERT INTO streamersettings (streamer, `key`, value) VALUES
-        (".$user->userid.",'icytags','".$db->escape($_POST['icy'])."')";
-        $db->execute($sql);
-    }else{
-        $sql = "DELETE FROM streamersettings WHERE streamer = ".$user->userid." AND `key` = 'icytags'LIMIT 1;";
+    if(isset($_POST['streampassword'])){
+        $sql = "UPDATE streamer SET streampassword = '".$db->escape($_POST['streampassword'])."' WHERE streamer = ".$user->userid." LIMIT 1;";
         $db->execute($sql);
     }
 }
-$sql = "SELECT * FROM streamersettings WHERE streamer = ".$user->userid;
+
+$sql = "SELECT * FROM streamer WHERE streamer = ".$user->userid." LIMIT 1";
 $dbres = $db->query($sql);
-while($row = $db->fetch($dbres)){
-    switch($row['key']){
-        case 'icytags';
-            $template['icytags'] = true;
-            break;
-        case 'defaultshowname':
-            $template['showname'] = $row['value'];
-            break;
-        case 'defaultshowdescription':
-            $template['showdesc'] = $row['value'];
-            break;
+if($dbres){
+    if($row = $db->fetch($dbres)){
+        $template['streampassword'] = $row['streampassword'];
     }
 }
 $template['PAGETITLE'] = 'Einstellungen';
