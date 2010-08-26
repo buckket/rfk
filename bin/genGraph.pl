5404d39f9d23ad2d5d51e430d79f07f58acc8ed9
MrLoom <MrLoom@soeinfachistdas.net>
2010-08-26 14:18:23 +0200

    New RRD Graphs

---
+++ bin/genGraph.pl
@@ -0,0 +1,125 @@
+#!/usr/bin/perl
+
+use RRDTool::OO;
+use strict;
+use warnings;
+
+chdir "/usr/share/rfk/var/lib/rrd";
+my $rrd = RRDTool::OO->new(file => "1.rrd" );
+
+$rrd->graph(
+	image => "/usr/share/rfk/www/graphh.png",
+	height => 120,
+	width => 500,
+	title => 'Hörerzahlen (Stunde)',
+	
+	start          => time() - 3600,
+    end            => time(),
+    
+	draw => {
+		file => "1.rrd",
+		name => "l1",
+		legend => "RfK MP3",
+		stack => 1,
+		type => "area",	
+		color => "FF9900",
+		cfunc => 'LAST',
+	},
+	draw => {
+		file => "2.rrd",
+		name => "l2",
+		legend => "RfK OGG",
+		stack => 1,
+		type => "area",
+		color => "0099FF",
+		cfunc => 'LAST',
+		
+	},
+	draw => {
+		file => "4.rrd",
+		name => "l3",
+		legend => "RfK OGG HQ",
+		stack => 1,
+		type => "area",
+		color => "99FF00",
+		cfunc => 'LAST',
+		
+	},
+);
+
+$rrd->graph(
+	image => "/usr/share/rfk/www/graphd.png",
+	height => 120,
+	width => 500,
+	title => 'Hörerzahlen (Tag)',
+
+	draw => {
+		file => "1.rrd",
+		name => "l1",
+		legend => "RfK MP3",
+		stack => 1,
+		type => "area",	
+		color => "FF9900",
+		cfunc => 'LAST',
+	},
+	draw => {
+		file => "2.rrd",
+		name => "l2",
+		legend => "RfK OGG",
+		stack => 1,
+		type => "area",
+		color => "0099FF",
+		cfunc => 'LAST',
+		
+	},
+	draw => {
+		file => "4.rrd",
+		name => "l3",
+		legend => "RfK OGG HQ",
+		stack => 1,
+		type => "area",
+		color => "99FF00",
+		cfunc => 'LAST',
+		
+	},
+);
+
+$rrd->graph(
+	image => "/usr/share/rfk/www/graphw.png",
+	height => 120,
+	width => 500,
+	title => 'Hörerzahlen (Woche)',
+	
+	start          => time() - 3600*24*7,
+    end            => time(),
+
+	draw => {
+		file => "1.rrd",
+		name => "l1",
+		legend => "RfK MP3",
+		stack => 1,
+		type => "area",	
+		color => "FF9900",
+		cfunc => 'LAST',
+	},
+	draw => {
+		file => "2.rrd",
+		name => "l2",
+		legend => "RfK OGG",
+		stack => 1,
+		type => "area",
+		color => "0099FF",
+		cfunc => 'LAST',
+		
+	},
+	draw => {
+		file => "4.rrd",
+		name => "l3",
+		legend => "RfK OGG HQ",
+		stack => 1,
+		type => "area",
+		color => "99FF00",
+		cfunc => 'LAST',
+		
+	},
+);
