a5b0ec8020b6acb3e338ef3d25583c22fc1704e5
The Chaos <gibtsnicht@kriegenwirauchnichtmehrre.in>
2010-10-06 23:27:23 +0200

    registrierung fixiert

--- www/register.php
+++ www/register.php
@@ -9,19 +9,19 @@ $template['PAGETITLE'] = $lang->lang('L_REGISTER');
 if(isset($_POST['action']) && $_POST['action'] == 'register'){
     $err = false;
     if($_POST['password'] != $_POST['password2']){
-        $_MSG['err'][] = 'Die Passwörter stimmen nicht überein';
+        $_MSG['err'][] = $lang->lang('L_ERR_PASSMISMATCH');
         $err = true;
     }
     if(strlen($_POST['username']) < 3){
-        $_MSG['err'][] = 'Der Benutzername muss mindestens 3 Zeichen enthalten';
+        $_MSG['err'][] = $lang->lang('L_ERR_USERNAME2SHORT');
         $err = true;
     }
     if(!$err){
         if($user->register($_POST['username'],$_POST['password']) == 0){
             $user->set_streampassword($_POST['streampassword']);
-            $_MSG['msg'][] = 'Erfolgreich registriert';
+            $_MSG['msg'][] = $lang->lang('L_REG_SUCCESS');
         }else{
-            $_MSG['err'][] = 'Fehler beim registrieren';
+            $_MSG['err'][] = $lang->lang('L_ERR_REGFAIL');
         }
     }else{
         $template['username'] = $_POST['username'];
