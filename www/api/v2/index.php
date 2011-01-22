87c6515275bd45cbc9edad839633d8d507a17f9c
teddydestodes <derkomtur@schattengang.net>
2011-01-22 20:33:21 +0100

    herp

--- www/api/v2/index.php
+++ www/api/v2/index.php
@@ -1,5 +1,9 @@
 <?php
 
+$basePath = dirname(dirname(dirname(dirname(__FILE__))));
+require_once $basePath.'/lib/common.inc.php';
+
+
 //TODO: die ganze quotascheiße
 require_once('api.inc.php');
 $api = new Api();
