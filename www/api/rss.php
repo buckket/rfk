888b462a5a46c49497cd8378688a0ddf9a32a272
teddydestodes <derkomtur@schattengang.net>
2011-06-16 17:55:07 +0200

    * fixed rss generator   it now generates more strict output

--- www/api/rss.php
+++ www/api/rss.php
@@ -2,15 +2,32 @@
 $basePath = dirname(dirname(dirname(__FILE__)));
 require_once $basePath.'/lib/common.inc.php';
 require_once $basePath.'/lib/api.inc.php';
-require_once $basePath.'/lib/RSS2Writer.php';
+require_once $basePath.'/lib/rss/FeedWriter.php';
 
-$rss = new RSS2Writer("RfK - Sendungen", "Sendungen", "http://radio.krautchan.net/",6,false);
 
-$rss->addCategory("RSS Feed");
+$Feed = new FeedWriter(RSS2);
+
+$title = 'Radio freies Krautchen';
+$link = 'http://radio.krautchan.net';
+$desc = 'Upcomming shows';
+$Feed->setTitle($title);
+$Feed->setLink($link);
+$Feed->setDescription($desc);
+
+$Feed->setImage($title,$link,$link.'/logo2.png');
+$Feed->setChannelElement('language', 'de-de');
+$Feed->setChannelElement('pubDate', date(DATE_RSS, time()));
 $out = array();
 getNextShows(&$out,500);
 
 foreach($out['shows'] as $show){
-    $rss->addItem($show['showdj'].': '.$show['showname'].' ('.date('d.m.Y H:i',$show['showbegin']).' - '.date('H:i',$show['showend']).')',$show['showdescription'],'http://radio.krautchan.net/broadcasts.php');
+  $newItem = $Feed->createNewItem();
+  $newItem->setTitle($show['showdj'].': '.$show['showname'].' ('.date('d.m.Y H:i',$show['showbegin']).' - '.date('H:i',$show['showend']).')');
+  $newItem->setLink('http://radio.krautchan.net/broadcasts.php');
+  $newItem->setDate($show['showbegin']);
+  $newItem->setDescription($show['showdescription']);
+  $newItem->addElement('author', $show['showdj']);
+
+  $Feed->addItem($newItem);
 }
-echo $rss->getXML();
+$Feed->genarateFeed();
