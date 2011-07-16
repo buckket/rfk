12d8c744d97aabe25e3560560a0a178470f80583
teddydestodes <derkomtur@schattengang.net>
2011-07-16 13:16:17 +0200

    + added modal loginbox + added pages to news page * newsboxes now have a max-length

--- lib/sites/News.php
+++ lib/sites/News.php
@@ -7,30 +7,47 @@ class News extends Site {
         $template->setTitle('News');
         $template->setTemplate('news.html');
         if(count($urlParams->getParams()) == 0) {
-            $template->addData('news',$this->getNews());
+            $template->addData('news',$this->getNews(1, $totalPages));
+            $template->addData('pages',$this->pageHandler(1, $totalPages));
         } else {
             $params = $urlParams->getParams();
             switch($params[0]) {
                 case 'post':
                     break;
                 default:
-                    $template->addData('news',$this->getNews(count($params) == 2?$params[1]:0));
+                    $template->addData('news',$this->getNews(count($params) == 2?$params[1]:1, $totalPages));
+                    $template->addData('pages',$this->pageHandler(count($params) == 2?$params[1]:1, $totalPages));
             }
         }
         return Site::$RENDER_TEMPLATE;
     }
 
-    private function getNews($page = 1) {
+    private function pageHandler($currpage,$totalPages){
+        $pages = array();
+        $url = new UrlParser('news');
+        if($currpage < $totalPages) {
+            $url->setParams(array('page',$currpage+1));
+            $pages['prev'] = $url->makeUrl();
+        }
+        if($currpage != 1) {
+            $url->setParams(array('page',$currpage-1));
+            $pages['next'] = $url->makeUrl();
+        }
+        return $pages;
+    }
+
+    private function getNews($page = 1,&$totalPages) {
         global $db;
         $pagesize = 5;
 
         $begin = (((int)$page)-1)*$pagesize;
         if($begin < 0)
             $begin = 0;
-        $sql = 'SELECT * FROM news JOIN streamer USING (streamer) LIMIT '.$begin.','.$pagesize;
+        $sql = 'SELECT SQL_CALC_FOUND_ROWS * FROM news JOIN streamer USING (streamer) LIMIT '.$begin.','.$pagesize;
         $dbres = $db->query($sql);
         $news = array();
         if ($dbres) {
+            $totalPages = ceil($db->getFoundRows()/$pagesize);
             while($item = $db->fetch($dbres)) {
                 $news[] = array('time' => $item['time'], 'username' => $item['username'], 'text' => $item['text']);
             }
--- www/js/rfk.js
+++ www/js/rfk.js
@@ -1,8 +1,22 @@
 $( function(){
 	$("#loginbox").hide();
+	$('#mask').click(function () {
+		$(this).hide();
+		$('.window').hide();
+	});
 });
 
-function showLoginBox(element){
-	$(element).hide();
-	$("#loginbox").show('slow');
+function showLoginBox() {
+	//Get the screen height and width
+	var maskHeight = $(document).height();
+	var maskWidth = $(window).width();
+	$('#mask').css({'width':maskWidth,'height':maskHeight});	
+	$('#mask').fadeTo("slow",0.8);
+	
+	var winH = $(window).height();
+	var winW = $(window).width();
+    var id= '#logindialog';
+	$(id).css('top',  winH/2-$(id).height()/2);
+	$(id).css('left', winW/2-$(id).width()/2);
+	$(id).fadeIn('fast');
 }
