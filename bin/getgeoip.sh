7002434ceecfcd0ee3d549ec2460039bdbc21739
MrLoom <MrLoom@soeinfachistdas.net>
2011-07-31 23:40:19 +0200

    path in getgeoip.sh fixed

--- bin/getgeoip.sh
+++ bin/getgeoip.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget -N -q http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz -O /tmp/GeoLiteCity.dat.gz
 
-zcat /tmp/GeoLiteCity.dat.gz > /usr/share/rfk/var/GeoLiteCity.dat
+zcat /tmp/GeoLiteCity.dat.gz > /home/radio/radio/var/GeoLiteCity.dat
