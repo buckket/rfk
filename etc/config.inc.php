4aacefc7504dc86395fc0e52c1535b7c21cd2941
teddydestodes <derkomtur@schattengang.net>
2011-08-30 23:33:42 +0200

    forgot config file

--- etc/config.inc.php
+++ etc/config.inc.php
@@ -2,7 +2,11 @@
 /**
  * RfK-Config
  */
-$_config['mysql-host'] = 'localhost';
+//base dir in filesystem mostyl used to find loopmusig and set paths for liquidsoap
+$_config['base'] = '/home/teddydestodes/src/rfk/';
+
+
+$_config['mysql-host'] = 'buchenwald';
 $_config['mysql-db']   = 'radio';
 $_config['mysql-user'] = 'radio';
 $_config['mysql-pass'] = 'muhmann';
@@ -11,15 +15,17 @@ $_config['template'] = 'radio';
 
 $_config['default-username'] = 'Gast';
 
-$_config['icecast_address'] = 'localhost';
+$_config['icecast_address'] = '192.168.2.101';
 $_config['icecast_external'] = '192.168.2.101';
 //$_config['icecast_external'] = 'radio.krautchan.net';
 $_config['icecast_port'] = 8000;
+$_config['icecast_pass'] = 'hackme';
 
-$_config['liquidsoap_address'] = '192.168.2.101';
+$_config['liquidsoap_address'] = '192.168.2.134';
 
-$_config['liquidsoap_port'] = 8010;
+$_config['liquidsoap_port'] = 8080;
 
 $_config['pagetitle'] = 'Radio freies Krautchan';
 $_config['webroot'] = '/rfk/';
+
 ?>
