c6ff5cb05b7c76d21af8acf3f2e286423ba73cb6
teddydestodes <derkomtur@schattengang.net>
2011-12-31 12:11:14 +0100

    added missing file

---
+++ www/listen.php
@@ -0,0 +1,31 @@
+<?php
+$basePath = dirname(dirname(__FILE__));
+require_once $basePath.'/lib/common.inc.php';
+if(!isset($_GET['stream'])) {
+    die('no stream selected');
+}
+
+$stream = (int)$_GET['stream'];
+if(!($stream > 0)) {
+    die('neindu!');
+}
+
+$sql = "SELECT * FROM mount_relay JOIN mounts USING (mount) JOIN relays USING (relay) WHERE mount = $stream";
+$dbres = $db->query($sql);
+$streams= array();
+if($dbres) {
+    while($m = $db->fetch($dbres)) {
+        $streams[] = array('name' => 'RfK @ '.$m['hostname'], 'url' => 'http://'.$m['hostname'].':8000'.$m['path']);
+    }
+}
+header('Content-type: audio/x-mpegurl');
+header('Content-Disposition: attachment; filename="rfk.m3u"');
+echo "#EXTM3U\r\n";
+foreach($streams as $stream) {
+    echo "#EXTINF:0, ".$stream['name']."\r\n";
+    echo $stream['url']."\r\n";
+}
+
+
+
+?>
