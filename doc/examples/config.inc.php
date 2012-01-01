262b944b5ea9f5f54c478f1657f0732f14cc34f5
teddydestodes <derkomtur@schattengang.net>
2012-01-01 17:57:54 +0100

    added doc dir

---
+++ doc/examples/config.inc.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * RfK-Config
+ */
+//database credentials
+$_config['mysql-host'] = 'localhost';
+$_config['mysql-db']   = 'radiodb';
+$_config['mysql-user'] = 'dbuser';
+$_config['mysql-pass'] = 'dbpassword';
+
+//template to use
+$_config['template'] = 'radio';
+
+//name of the not logged in user
+$_config['default-username'] = 'Gast';
+
+//address of the streaming server
+$_config['icecast_address'] = '172.0.0.1';
+$_config['icecast_port'] = 8000;
+
+//addess for liquidsoap
+$_config['liquidsoap_address'] = '172.0.0.1';
+$_config['liquidsoap_port'] = 8010;
+
+//proshow loop config
+$_config['preshow_time'] = 7*60; // time before pre show loop in seconds
+$_config['preshow_loop'] = 'null';
+
+$_config['base'] = '/path_to/rfk';
+//lastfm credentials set to null if disabled
+$_config['lastfm'] = array('username','password');
+
+//absolut path to record archive
+$_config['recorddir'] = '/tmp/';
+?>
