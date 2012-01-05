6ee1f20b4b1aac78bee46286ace5825dd4edf8ea
teddydestodes <derkomtur@schattengang.net>
2012-01-05 23:23:09 +0100

    added remote_query to traffic.php

--- bin/traffic.php
+++ bin/traffic.php
@@ -2,18 +2,49 @@
 require_once(dirname(__FILE__).'/../lib/common.inc.php');
 error_reporting(0); // disable error reporting
 
-// Set this value accordingly (1 = Master)
-$relay = 1;
 
-$traffic = parseVNStat();
-adjustUnit(&$traffic['out']);
-if (updateDatabase($traffic['out']['value'], $relay)) {
-    return 0;
-}
-else {
-    return 1;
+$sql = "SELECT relay, hostname, port, query_method, query_user, query_pass FROM relays WHERE query_method <> 'NO_QUERY'";
+$dbres = $db->query($sql);
+$somefail = false;
+if($dbres && $db->num_rows($dbres)) {
+    while($server = $db->fetch($dbres)) {
+        $traffic = array();
+        switch($server['query_method']) {
+            case 'REMOTE_ICECAST2_KH':
+                if(($traffic = getRemoteStat($server['hostname'], $server['port'], $server['query_user'], $server['query_pass'])) === false) {
+                    $somefail = true;
+                    continue;
+                }
+                break;
+            case 'LOCAL_VNSTAT':
+                $traffic = parseVNStat();
+                break;
+        }
+        adjustUnit(&$traffic['out']);
+        if(!updateDatabase($traffic['out']['value'], $server['relay'])){
+            $somefail = true;
+        }
+    }
 }
 
+if($somefail) {
+    exit(1);
+} else {
+    exit(0);
+}
+
+function getRemoteStat($ip,$port,$username,$password) {
+    $tmp = array();
+
+    $page = file_get_contents('http://'.$username.':'.$password.'@'.$ip.':'.$port.'/admin/status.xml');
+    if(preg_match('/<outgoing_kbitrate>(\\d+)<\\/outgoing_kbitrate>/', $page, $matches)){
+        $tmp['out']['value'] = $matches[1];
+        $tmp['out']['unit'] = 'KiBit';
+    } else {
+        return false;
+    }
+    return $tmp;
+}
 
 function parseVNStat() {
     $str = file_get_contents('../var/vnstat');
@@ -36,6 +67,9 @@ function adjustUnit(&$traffic) {
             break;
         case 'MiB':
             $traffic['value'] = round(floatval($traffic['value']) * (2^20) / (10^6));
+            break;
+        case 'KiBit':
+            $traffic['value'] = round(floatval($traffic['value']) / 8);
             break;
     }
 }
