69d5c7c5495990ee1f992e9e2785c71ba0cb593c
The Chaos <git_ist_ein@hurren_son.org>
2012-01-08 14:40:40 +0100

    add locales*.sql

---
+++ src/locales-2012-01-08.sql
@@ -0,0 +1,18 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+INSERT INTO `locales` VALUES
+(1,"Europe/Berlin","de","de","German","Deutsch"),
+(3,"CEST","en","us","English","English"),
+(4,"Europe/Berlin","en","uk","English","English"),
+(5,"Europe/Berlin","en","texas","English","English"),
+(6,"Europe/Berlin","nl","nl","Dutch","Nederlands"),
+(7,"Europe/Warsaw","ru","ru","Russian","Русский"),
+(8,"Asia/Tokyo","jp","jp","Japanese","日本語"),
+(10,"Europe/Berlin","it","it","Italian","Italiano"),
+(11,"Europe/Berlin","dk","dk","Danish","Dansk"),
+(12,"Europe/Berlin","pl","pl","Polish","Polski"),
+(13,"Europe/Berlin","fr","fr","French","Français"),
+(14,"Europe/Berlin","gr","gr","Greek","Ελληνικά"),
+(15,"Europe/Berlin","ee","ee","Estonian","Eesti"),
+(16,"Europe/Berlin","de","ch","German","Deutsch"),
+(17,"Europe/Berlin","de","at","German","Deutsch");
